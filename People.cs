fc68515a36bd23631246331d898757c559fc413f
petrus <pshai671@gmail.com>
2022-11-30 10:52:16 +0200

    Add project files.

---
+++ People.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Object_oriented_program
+{
+     class People
+    {
+        public string Name;
+        public string Gender;
+        public string Race;
+        public int  Age;
+
+        public People (string PersonName,string PersonGender,string PersonRace, int PersonAge)
+        {
+            Name = PersonName;
+            Gender = PersonGender;
+            Race = PersonRace;
+            Age = PersonAge;
+
+
+
+        }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using Object_oriented_program;
+using System.ComponentModel.DataAnnotations;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        People person1 = new People("PETRUS", "MALE", "BLACK", 23);
+        People person2 = new People("PRECIOUS", "FEMALE", "BLACK", 22);
+        People person3 = new People("DINEO", "FEMALE", "BLACK", 16);
+
+        Console.WriteLine(person1.Name + " " + person1.Gender + " " + person1.Race + " " + person1.Age);
+        Console.WriteLine(person2.Name + " " + person2.Gender + " " + person2.Race + " " + person2.Age);
+        Console.WriteLine(person3.Name + " " + person3.Gender + " " + person3.Race + " " + person3.Age);
+
+    }
+}
